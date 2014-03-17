483e2f05ed284bd5465b07cc4322f29cd7ef9691
Rini <rini.cr@experionglobal.com>
2014-03-17 14:23:40 +0530

    Test Folder Build Folder

---
+++ module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class ApplicationControllerTest extends AbstractHttpControllerTestCase
+{
+	//protected $traceError = true;
+   
+     public function setUp()
+    {
+        $this->setApplicationConfig(
+            include 'TestConfig.php'
+        );
+        parent::setUp();
+    }
+    public function testIndexActionCanBeAccessed()
+	{
+	  
+	    $this->dispatch('/');
+		$this->assertResponseStatusCode(200);
+	   
+
+	}
+
+	
+}
